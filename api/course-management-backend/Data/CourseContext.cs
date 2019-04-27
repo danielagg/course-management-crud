dc8da8441aafd4097cb0690d48f1d3bb2f13a099
Dániel Agg <daniel.agg@outlook.com>
2019-04-27 15:12:04 +0200

    Initial structure for backend with dummy data

---
+++ api/course-management-backend/Data/CourseContext.cs
@@ -0,0 +1,23 @@
+﻿using course_management_backend.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace course_management_backend.Data
+{
+    public class CourseContext : DbContext
+    {
+        public CourseContext(DbContextOptions<CourseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+        }
+    }
+}
---
+++ api/course-management-backend/Data/DbInitializer.cs
@@ -0,0 +1,37 @@
+﻿using course_management_backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace course_management_backend.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(CourseContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Courses.Any())
+                return;   // DB has been seeded
+
+            var courses = new Course[]
+            {
+                new Course{ Name = "Chemistry" },
+                new Course{ Name = "Microeconomics" },
+                new Course{ Name = "Macroeconomics" },
+                new Course{ Name = "Calculus" },
+                new Course{ Name = "Trigonometry" },
+                new Course{ Name = "Composition" },
+                new Course{ Name = "Literature" }
+            };
+
+            foreach (Course c in courses)
+            {
+                context.Courses.Add(c);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
